194124eca5bfafd70a14f11041bcf89b7aaa3202
adianali <adianalipratama98@gmail.com>
2023-06-15 02:15:33 +0700

    membuat detail dari obat obatan, button obat obatan, responsive pada page obat obatan, alat kesehatan,contact us dan about us

--- frontEnd/src/scripts/globals/api-endpoint.js
+++ frontEnd/src/scripts/globals/api-endpoint.js
@@ -3,7 +3,7 @@ import CONFIG from './config';
 const API_ENDPOINT = {
   HOME: `${CONFIG.BASE_URL}`,
   MEDICINE: `${CONFIG.MEDICINE}`,
-  DETAIL: (id) => `${CONFIG.BASE_URL}detail/${id}`,
+  DETAIL_MEDICINE: (slug) => `${CONFIG.DETAIL_MEDICINE}detail/${slug}`,
   REKOMENDASI: `${CONFIG.REKOMENDASI}`,
 };
 
--- frontEnd/src/scripts/views/pages/detail-obat.js
+++ frontEnd/src/scripts/views/pages/detail-obat.js
@@ -1,14 +1,29 @@
 import UrlParser from '../../routes/url-parser';
+import YoungHealthSource from '../../data/younghealth-source';
+import { createDetailObatTemplate } from '../templates/template-creator';
+
 
 const DetailObat = {
   async render() {
     return `
-    <h1>ini halaman detail</h1>
+    <section class="detail-obat">
+        <div class="container-detail-obat">
+          <div class="row" id="info-detail-obat">
+              
+          </div>
+        </div>
+    </section>
     `;
   },
 
   async afterRender() {
-   
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const obat = await YoungHealthSource.detailObat(url.id);
+    console.log(obat);
+
+    const detailContainer = document.querySelector('#info-detail-obat');
+
+    detailContainer.innerHTML = createDetailObatTemplate(obat);
   },
 };
 
